e8514b6f62a0560e89a1ccaddd60bbd90da2092b
dmazurki <damianmazurkiewicz94@gmail.com>
2015-04-09 23:13:54 +0200

    main

---
+++ Minesweeper/src/Controller/Controller.java
@@ -0,0 +1,23 @@
+/**
+ * Controller class.
+ */
+
+package Controller;
+
+import javax.swing.SwingUtilities;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run()
+			{
+				
+			}
+		});
+
+	}
+
+}
